08d6b64a4f67a446543e3360edeb8e245949db93
Cathal Garvey <cathalgarvey@cathalgarvey.me>
2013-03-06 14:06:35 +0000

    Ugly and in need of cleaning, but appears to work.

--- TinyStatus.py
+++ TinyStatus.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 for x in ('itertools takewhile count:os.path exists:hashlib sha256:time time ct'
-'ime:json dump dumps load loads:sys argv:xmlrpc.client ServerProxy:xmlrpc.serve'
-'r SimpleXMLRPCServer:re search').split(":"):
+'ime:json dump dumps load loads:sys argv exit:xmlrpc.client ServerProxy:xmlrpc.'
+'server SimpleXMLRPCServer:re search').split(":"):
  exec('from {0} import {1}'.format(x.split()[0],','.join(x.split()[1:])))
 # Above codeblock equivalent to:
 #from itertools import takewhile, count
@@ -24,9 +24,12 @@ if not exists("sd"):(lambda f:(dump([[],[],[]],f),f.close()))(open("sd",'w'))
 # O is "output", dumps [[Peers],[follows],[messages]] to file "sd".
 # Setting Class attribute M to empty dict allows instances to just assume it's there, avoiding attributeerrors.
 # Because instance self.M is set in init after first use of self.A, it's no longer referring to the class attribute.
+# Message regeneration functions.
+MfD,MfL=lambda z:M(z['n'],z['m'],z['t'],z['v']),lambda z:M(z[1],z[2],z[0],z[3])
+
 class D:O,M=lambda s:dump([s.P,s.F,[s.M[m].r() for m in s.M]],open("sd","w")),{}
 # Init sets s.P to Peers, s.F to follows, calls s.A([]) to regenerate message dict and pass to s.M.
-def Di(s,B): s.P,s.F,s.M=B[0],B[1], s.A([M(x[1],x[2],x[0],x[3]) for x in B[2]])
+def Di(s,B): s.P,s.F,s.M=B[0],B[1], s.A([MfL(x) for x in B[2]])
 # A returns a dict of message hashes:messages, if they show proof of work.
 # It does this by converting the existing s.M dict into a list of [key,value] pairs,
 # and adds this to the new proposed list of [key,value] pairs, then processes them all.
@@ -41,18 +44,9 @@ D.S=lambda s,hl=[]:(s.O() or sorted([s.M[m] for m in s.M if m in (hl or s.M)]))
 #D.S=lambda s,hl=[]:sorted([s.M[m] for m in s.M if m in (hl or s.M)])
 # Search DB by regex! "G" is for "Get"
 D.G=lambda s,g=0:[x for x in filter(lambda n:(g==0)or search(g,str(n)),s.S())]
-# Need handlers for Servers; this adds & returns current list.
-# def pr(x=[]): return ([(y in prs) or prs.append(y) for y in x] or 1) and prs
+# Need handlers for Servers; this adds & returns current list, checking if server
+# already in list.
 D.p=lambda s,n=[]:([(y in s.P) or s.P.append(y) for y in n] or 1) and s.P
-# Can't keep track of which servers are good and bad, as it'd lead to different entries between peer servers:
-# the assessment metadata would lead to servers accepting duplicates of known servers thinking them unique because
-# of peer markup.
-# Instead, we'll pop and insert(0, foo) foo when it is successfully reached, so that successful peers
-# are always tried first.
-# So, a method that code can call back to the database to say "this server worked": (Pr=Peer Recommend)
-# Finds, pops and inserts peer into first position in peer list.
-# NB: So far this is nowhere in use.
-D.Pr=lambda s,n:s.P.insert(s.P.pop(s.P.index(n)))
 
 class M:N,r=lambda s,x:SA(s,'v',x),lambda s,x=0:[s.t,s.n,s.m[:150],x or s.v]
 def mi(s,n,m,t=0,v=0):s.t,s.n,s.m,s.v=t or int(time()),n,m[:150],v # Msg Format: [time,nick,msg,nonce]
@@ -61,17 +55,20 @@ def mH(s,n=0):return sha256(bytes(dumps(s.r(n)),'utf8')).hexdigest()
 Rs,POW,M.H,M.__init__="@{}, on {}: {}","001",mH,mi
 M.__lt__,M.__repr__=lambda s,o:s.t<o.t,lambda s:(Rs).format(s.n,ctime(s.t),s.m)
 M.P=lambda s:[s.N(x+1) for x in takewhile(lambda x:s.H(x)>POW,count())].pop()
-# Must be x+1 as takewhile doesn't give the last, working number. pop() at end prevents returning as value is None.
+# ^Must be x+1 as takewhile doesn't give the last, working number.
+#  pop() at end prevents returning as value is None.
 
 DB,SP=D(load(open("sd"))),ServerProxy
-I,SXS=lambda L:SA(DB,'M',DB.A([M(x[1],x[2],x[0],x[3]) for x in L])),SimpleXMLRPCServer
+# I returns 1 always, so that ServerProxy doesn't throw a fit on Server's behalf
+# at being asked to return None.
+I,SXS=lambda L:(SA(DB,'M',DB.A([MfD(x) for x in L]))or 1),SimpleXMLRPCServer
 # s usage:
 # s(0,a): returns DB.p(a) => Appends peer list a and returns full peer list. --> Works!
 # s(1,a): returns DB.G(a) => Returns list of hashes for messages matching regex a.
 # s(2,a): returns DB.S(a) => Returns list of messages whose hashes are in list a, OR all messages in stream if a empty.
 # s(3+,a): calls I(a) (=>DB.A(a) with message instantiation) and returns DB.O() (Which saves the current DB)
-s=lambda n,a:((n<1 and DB.p(a))or(n==1 and DB.G(a))or(n==2 and DB.S(a))or(I(a)) and DB.O())
-# Usage similar to TinyP2P:
+s=lambda n,a:((n<1 and DB.p(a))or(n==1 and DB.G(a))or(n==2 and DB.S(a))or I(a))
+ # Usage similar to TinyP2P:
 # $ python3 tinystatus server <external ip or hostname> <portnum> <otherservers...>
 # Poll fetches messages from remote servers and prints them.
 # $ python3 tinystatus update <servers...>
@@ -81,6 +78,7 @@ s=lambda n,a:((n<1 and DB.p(a))or(n==1 and DB.G(a))or(n==2 and DB.S(a))or(I(a)) 
 # $ python3 tinystatus unfollow <stuff..>
 # $ python3 tinystatus search <string> <servers..>
 print(argv)
+if len(argv)==1:print("Use:\nTinyStatus.py server hostname serverport [otherurls]\nTinyStatus.py client serverurl [Username Message (do not include if fetching.)]")or exit(1)
 if "serve" in argv[1].lower():
  # TinyP2P Server Bootstrap:
  # myU,prs,srv = ("http://"+ar[3]+":"+ar[4], ar[5:],lambda x:x.serve_forever())
@@ -88,33 +86,38 @@ if "serve" in argv[1].lower():
  # pr() and [aug(s) for s in aug(pr()[0])]
 
  # Database has a "P" key for peers, instead of a floating "prs" variable, and "p" method identical to pr()
- U,P,sv="http://"+argv[2]+":"+argv[3],DB.p(argv[4:]),lambda x:x.serve_forever()
+ # U is the URL, but before returning the URL the below expression also incorporates
+ # command line otherurls into DB.P.
+ U,sv=DB.p(argv[4:])or "http://"+argv[2]+":"+argv[3],lambda x:x.serve_forever()
  #f=lambda p,n,a:(p==pw(myU))and(((n==0)and pr(a))or((n==1)and [ls(a)])or c(a))
  # n=mode: mode 0 is fetch peers, mode 1 is fetch hashes by keyword, mode 2 is fetch messages by hash list.
  # Mode 3+ means "post".
- # Will this die the server? Do we need an aux?
- if argv[4:]: foo=[DB.p(SP(x).s(0,D.p())) for x in D.p()[:]]
- #(lambda sv:sv.register_function(f,"f") or srv(sv))(xs((ar[3],int(ar[4]))))
+ # This DOESN'T work:
+ #argv[4:] and [DB.p(SP(0,x).s(0,DB.p())) for x in DB.p()[:]]
+ # The below equates to "if argv[4:]: blah".
+ # DB.p(SP(x).s(0,[DB.p()])) means "Add the result from server X's s(0,[my current list of servers]) function to DB.P.
+ argv[4:] and [DB.p(SP(x).s(0,[U]+[DB.p()])) for x in argv[4:]]
+# if argv[4:]: foo=[DB.p(SP(x).s(0,D.p())) for x in D.p()[:]]
  DB.p([U])
  (lambda E:E.register_function(s,"s") or sv(E))(SXS((argv[2],int(argv[3]))))
-elif argv[1][:3] == "fol": DB.F.extend(argv[2:])
-elif "unf" in argv[1]: foo=[DB.F.remove(x) and DB.O() for x in argv[2:]]
-elif "post" in argv[1]:
- # Use list iteration to accomplish message creation, Proof-of-work and json export.
- #MyMsg = [x.P() and x.r() for x in [M(argv[2],argv[3])]].pop()
- MyMsg = M(argv[2],argv[3])
- MyMsg.P()
- MyMsg = MyMsg.r()
- print(MyMsg)
- for url in SP(argv[4]).s(0,DB.p()):
-  SP(url).s(3,MyMsg)
 else:
- print("Stream mode")
- for url in SP(argv[1]).s(0,DB.p()):
+# Usage client serverurl [Username Message (do not include if fetching.)]
+ MyMsg=[]
+ if len(argv[2:]) == 2:
+  MyMsg = M(argv[2],argv[3])
+  MyMsg = MyMsg.P() or MyMsg.r()
+ for url in SP(argv[1]).s(0,DB.P):
+  SP(argv[1]).s(3,MyMsg)
+  # I, defined above, replaces DB.M with a new dict containing new and old messages.
+  #print(SP(argv[1]).s(1,''))
+#  print(SP(argv[1]).s(1,''))
+  I(SP(argv[1]).s(1,''))
+ print('\n'.join([repr(x) for x in DB.S()]))
+# for url in SP(argv[1]).s(0,DB.P):
   # For each followed keyword/user/search:
   # Get list of matching hashes, and if it's not a known hash, append to a list,
   # then dump the list in a message request by hash to the server.
-  for kw in DB.F: print(kw)
-  for kw in DB.F: DB.A(SP(url).s(2,[x for x in SP(url).s(1,kw)if x not in DB.M]))
- print(DB.S())
+#  for kw in DB.F: print(kw)
+#  for kw in DB.F: DB.A(SP(url).s(2,[x for x in SP(url).s(1,kw)if x not in DB.M]))
+# print(DB.S())
 DB.O()
